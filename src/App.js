253089e8e9fc191898ccb48e8c41251175c42f7c
Valen <v.lunaklick@gmail.com>
2021-06-27 12:17:31 -0300

    Points header solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
+  const [points,setPoints] = useState(0);
+  
+  const [bestscore,setBestscore] = useState(points);
+
+
   return (
-    <Header />
+    <>
+      <Header points={points} bestscore={bestscore} />
+      <Main />
+    </>
   );
 }
 
---
+++ src/components/Main/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './main.css'
+
+const Main = (props) => {
+    return(
+        <main className="mainCont"> 
+
+            <h2 className="subtitle">You get a point if you select one you havent't yet</h2>
+
+            <div className="wrapper">
+
+            </div>
+
+        </main>
+    )
+}
+
+export default Main
