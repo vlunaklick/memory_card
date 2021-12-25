488b81c3e0e594b4fe8923d168ccfe7670b8977c
Valen <v.lunaklick@gmail.com>
2021-12-25 19:23:46 -0300

    Changed colors and removed console logs

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import { useEffect, useState } from 'react'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 
 function App() {
-  const [points,setPoints] = useState(0);
-  
-  const [bestscore,setBestscore] = useState(points);
+	const [points, setPoints] = useState(0)
 
+	const [bestscore, setBestscore] = useState(points)
 
-  useEffect(() =>{
-    setPoints(0);
-  }, [])
+	useEffect(() => {
+		setPoints(0)
+	}, [])
 
+	useEffect(() => {
+		if (points > bestscore) {
+			setBestscore(points)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [points])
 
-  useEffect(() => {
-    if (points > bestscore){
-      setBestscore(points)
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[points])
+	const increment = () => {
+		setPoints(prevPoints => {
+			return prevPoints + 1
+		})
+	}
 
-  const increment = () =>{
-    setPoints(prevPoints =>{
-      return prevPoints + 1
-    });
-  };
+	const reset = () => {
+		setPoints(0)
+	}
 
-  const reset = () =>{
-    setPoints(0);
-  };
-
-  return (
-    <>
-      <Header points={points} bestscore={bestscore} />
-      <Main increment={increment} reset={reset} />
-    </>
-  );
+	return (
+		<>
+			<Header points={points} bestscore={bestscore} />
+			<Main increment={increment} reset={reset} />
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,87 +4,138 @@ import './main.css'
 
 import PicturesT from '../Pictures/Pictures'
 
-function Main({increment,reset}){
+function Main({ increment, reset }) {
+	let [perso, setPerso] = useState(personajes)
+	let [selected, setSelected] = useState(chequeo)
 
-    let [perso, setPerso] = useState(personajes)
-    let [selected, setSelected] = useState(chequeo);
+	function shuffle(array) {
+		return array.sort(() => Math.random() - 0.5)
+	}
 
-    function shuffle(array) {
-        return array.sort(() => Math.random() - 0.5);
-    }
+	useEffect(() => {
+		setPerso(() => {
+			return [...shuffle(perso)]
+		})
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
-    useEffect(()=>{
-        setPerso(()=> {return [...shuffle(perso)]})
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
+	const handleClick = key => {
+		setPerso(() => {
+			return [...shuffle(perso)]
+		})
+		if (selected[key] === true) {
+			setSelected(chequeo)
+			reset()
+		} else {
+			setSelected(prevSel => {
+				return {
+					...prevSel,
+					[key]: !prevSel[key],
+				}
+			})
+			increment()
+		}
+	}
 
-    const handleClick = (key) => {
-        setPerso(()=> {return [...shuffle(perso)]});
-        if (selected[key] === true){
-            setSelected(chequeo)
-            reset();
-        } else{
-            setSelected(prevSel =>{
-                console.log(selected)
-                console.log(prevSel[key])
-                return {
-                        ...prevSel,
-                        [key]: !prevSel[key]
-                        }
-            })
-            increment();
-        }
-    }
+	let cartas = perso.map(item => {
+		return (
+			<PicturesT
+				handleClick={handleClick}
+				key={item.id}
+				src={item.src}
+				name={item.name}
+				id={item.id}
+			/>
+		)
+	})
 
-    let cartas = perso.map(item => {
-        return <PicturesT  handleClick={handleClick} key={item.id} src={item.src} name={item.name} id={item.id} />
-    });
+	return (
+		<main className='mainCont'>
+			<h2 className='subtitle'>
+				You get a point if you select one you havent't yet
+			</h2>
 
-    return(
-
-        <main className="mainCont"> 
-
-            <h2 className="subtitle">You get a point if you select one you havent't yet</h2>
-
-            <div className="wrapper">
-                
-                {cartas}
-
-            </div>
-
-        </main>
-    )
+			<div className='wrapper'>{cartas}</div>
+		</main>
+	)
 }
 
 export default Main
 
-
 let personajes = [
-    {src:"https://pbs.twimg.com/media/E3MQRLCXoAAqhT7.jpg:large", name: "Hisoka", id: "hisoka"}, 
-    {src:"https://www.tierragamer.com/wp-content/uploads/2019/08/Hunter-x-Hunter-Gon-450x300.jpg", name: "Gon", id:"gon"},
-    {src:"https://i.pinimg.com/originals/27/ca/b9/27cab9588503c762f88e6311751ebddf.jpg", name: "Kurapika", id:"kurapika"},
-    {src:"https://pbs.twimg.com/media/EvHYsbtWgAgjJ6S.jpg",name:"Chrollo",id:"chrollo"},
-    {src:"https://www.radiodemente.cl/wp-content/uploads/2020/04/Leorio.jpg",name:"Leorio",id:"leorio"},
-    {src:"https://sm.ign.com/t/ign_latam/screenshot/default/meruem2_sb8r.1280.jpg",name:"Meruem",id:"meruem"},
-    {src:"https://i.pinimg.com/736x/33/25/b3/3325b3ab5ee08ded70e1454656467f4c.jpg",name:"Killua",id:"killua"},
-    {src:"https://i.pinimg.com/474x/b3/46/9b/b3469b100b8c0e042ad96e34ed8f7b85.jpg",name:"illumi",id:"illumi"},
-    {src:"https://pm1.narvii.com/6297/77de566ac5af1f2352defbb54e853324073e6dd2_hq.jpg",name:"Ging",id:"ging"},
-    {src:"https://areajugones.sport.es/wp-content/uploads/2020/04/netero.jpg",name:"Netero",id:"netero"},
-    {src:"https://assets.puzzlefactory.pl/puzzle/286/478/original.jpg",name:"Feitan",id:"feitan"},
-    {src:"https://i.ytimg.com/vi/-aQ9xbVqjYw/maxresdefault.jpg",name:"Razor",id:"razor"},
-];
+	{
+		src: 'https://pbs.twimg.com/media/E3MQRLCXoAAqhT7.jpg:large',
+		name: 'Hisoka',
+		id: 'hisoka',
+	},
+	{
+		src: 'https://www.tierragamer.com/wp-content/uploads/2019/08/Hunter-x-Hunter-Gon-450x300.jpg',
+		name: 'Gon',
+		id: 'gon',
+	},
+	{
+		src: 'https://i.pinimg.com/originals/27/ca/b9/27cab9588503c762f88e6311751ebddf.jpg',
+		name: 'Kurapika',
+		id: 'kurapika',
+	},
+	{
+		src: 'https://pbs.twimg.com/media/EvHYsbtWgAgjJ6S.jpg',
+		name: 'Chrollo',
+		id: 'chrollo',
+	},
+	{
+		src: 'https://www.radiodemente.cl/wp-content/uploads/2020/04/Leorio.jpg',
+		name: 'Leorio',
+		id: 'leorio',
+	},
+	{
+		src: 'https://sm.ign.com/t/ign_latam/screenshot/default/meruem2_sb8r.1280.jpg',
+		name: 'Meruem',
+		id: 'meruem',
+	},
+	{
+		src: 'https://i.pinimg.com/736x/33/25/b3/3325b3ab5ee08ded70e1454656467f4c.jpg',
+		name: 'Killua',
+		id: 'killua',
+	},
+	{
+		src: 'https://i.pinimg.com/474x/b3/46/9b/b3469b100b8c0e042ad96e34ed8f7b85.jpg',
+		name: 'illumi',
+		id: 'illumi',
+	},
+	{
+		src: 'https://pm1.narvii.com/6297/77de566ac5af1f2352defbb54e853324073e6dd2_hq.jpg',
+		name: 'Ging',
+		id: 'ging',
+	},
+	{
+		src: 'https://areajugones.sport.es/wp-content/uploads/2020/04/netero.jpg',
+		name: 'Netero',
+		id: 'netero',
+	},
+	{
+		src: 'https://assets.puzzlefactory.pl/puzzle/286/478/original.jpg',
+		name: 'Feitan',
+		id: 'feitan',
+	},
+	{
+		src: 'https://i.ytimg.com/vi/-aQ9xbVqjYw/maxresdefault.jpg',
+		name: 'Razor',
+		id: 'razor',
+	},
+]
 
 let chequeo = {
-    hisoka: false,
-    gon: false,
-    kurapika: false,
-    chrollo: false,
-    leorio: false,
-    meruem: false,
-    killua: false,
-    illumi: false,
-    ging: false,
-    netero: false,
-    feitan: false,
-    razor: false
+	hisoka: false,
+	gon: false,
+	kurapika: false,
+	chrollo: false,
+	leorio: false,
+	meruem: false,
+	killua: false,
+	illumi: false,
+	ging: false,
+	netero: false,
+	feitan: false,
+	razor: false,
 }
